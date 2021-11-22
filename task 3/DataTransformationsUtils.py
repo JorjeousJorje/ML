7df502c582548c5c6ac9e96028d684718786367d
JorjeousJorje <g.bakharev@g.nsu.ru>
2021-11-22 13:08:47 +0300

    added task 4
    heh

--- task 3/DataTransformationsUtils.py
+++ task 3/DataTransformationsUtils.py
@@ -3,21 +3,12 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class UnusedPixelsRemover(BaseEstimator, TransformerMixin):
-    
-    def get_removing_mask(self, X: np.ndarray) -> np.ndarray:
-        mask: np.ndarray = np.zeros_like(X[0])
-    
-        for sample in X:
-            mask = (mask != sample)
-        
-        return mask
-    
+
     def __init__(self) -> None:
         self.removing_mask: np.ndarray = None
     
-    
     def fit(self, X: np.ndarray, y: np.ndarray=None):
-        self.removing_mask = self.get_removing_mask(X)
+        self.removing_mask = np.sum(X, axis=0,  dtype=np.bool)
         return self
         
     def transform(self, X: np.ndarray) -> np.ndarray:
