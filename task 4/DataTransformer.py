bac4682647b158a3dc3be73637fcc62db52b7b0d
JorjeousJorje <g.bakharev@g.nsu.ru>
2021-12-14 18:00:31 +0300

    heh
    hah

--- task 4/DataTransformer.py
+++ task 4/DataTransformer.py
@@ -1,19 +1,16 @@
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, RobustScaler
 
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
+import scipy.stats as stats
 
-from DataProcessingUtils import *
+
 
 class DataTransformer:
-    
     
     def __init__(self, scaler=None):
         if scaler is not None:
@@ -21,19 +18,69 @@ class DataTransformer:
         else:
             self.scaler = StandardScaler()
 
-        self.imputer = IterativeImputer(max_iter=10,
-                                        random_state=0)
-    
+        self.encoder = LabelEncoder()
+        self.imputer = IterativeImputer(max_iter=10, random_state=0)
+        
+    def fillnans(self, X: pd.DataFrame):
+        pd.options.mode.chained_assignment = None
+        
+        # fill with mode
+        X['Exterior1st'] = X['Exterior1st'].fillna(X['Exterior1st'].mode()[0])
+        X['Exterior2nd'] = X['Exterior2nd'].fillna(X['Exterior2nd'].mode()[0])
+        X['MSZoning'] = X['MSZoning'].fillna(X['MSZoning'].mode()[0])
+        
+        dist_cols = ["LotFrontage", "GarageYrBlt"]
+        
+        for col in dist_cols:
+            missing = X[X[col].isna()][col]
+            not_missing = X[X[col].notnull()][col]
+
+            params = stats.johnsonsu.fit(not_missing)
+
+            r = stats.johnsonsu.rvs(params[0], params[1], params[2], params[3], size=missing.shape[0])
+            X[col].loc[missing.index] = r
+
+            
+        zero_nan_cols = ['GarageArea', 'GarageCars', "MasVnrArea", 'BsmtFinSF1', 'BsmtFinSF2',
+                            'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath', 'BedroomAbvGr',
+                            "1stFlrSF", "2ndFlrSF"]
+        
+        X[zero_nan_cols] = X[zero_nan_cols].fillna(0)
+        
+        X['Functional'] = X['Functional'].fillna('Typ')  # Typical Functionality
+        X['Electrical'] = X['Electrical'].fillna("SBrkr")  # Standard Circuit Breakers & Romex
+        X['KitchenQual'] = X['KitchenQual'].fillna("TA")  # Typical/Average
+        
+        rest_cols = X.select_dtypes("object").fillna("None").astype("object")
+        X[rest_cols.columns] = rest_cols.values
+        
+        return X
+            
+        
     def prepare(self, X):
-        X = delete_nans(X, 0.45)
-        X = fill_nans(X)
+        X = self.fillnans(X)
         return X
     
-    def fit_transform(self, X):
+    def encode(self, X):
+        object_candidates = list(X.dtypes[X.dtypes == "object"].index.values)
+        
+        for col in object_candidates:
+            X[col] = self.encoder.fit_transform(X[col])
+        
+        return X
+    
+    def fit_transform(self, X, obj_to_num: bool=False):
         
         X = self.prepare(X)
         num_candidates = list(X.dtypes[X.dtypes != "object"].index.values)
         X[num_candidates] = self.imputer.fit_transform(X[num_candidates])
-        X[num_candidates] = self.scaler.fit_transform(X[num_candidates])   
+        
+        X['TotalSF'] = X['TotalBsmtSF'] + X['1stFlrSF'] + X['2ndFlrSF']
+        X['Total_sqr_footage'] = (X['BsmtFinSF1'] + X['BsmtFinSF2'] + X['1stFlrSF'] + X['2ndFlrSF'])
+        X['Total_porch_sf'] = (X['OpenPorchSF'] + X['3SsnPorch'] + X['EnclosedPorch'] + X['ScreenPorch'] + X['WoodDeckSF'])
+        
+        if obj_to_num:
+            X = self.encode(X)
+            
         return X     
     
--- task 4/TrainUtils.py
+++ task 4/TrainUtils.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+import pandas as pd
 from sklearn.metrics import mean_squared_error
 
 def rmsle(y, y_pred):
@@ -7,4 +9,41 @@ def rmsle(y, y_pred):
 def evaluate(model, X, y):
     preds = model.predict(X)
     print("RMSLE: " + str(rmsle(preds, y)))
+    
+    
+    
+def to_categorical(X):
+    for c in X.columns:
+        col_type = X[c].dtype
+        if col_type == 'object' or col_type.name == 'category':
+            X[c] = X[c].astype('category')
+
+def submission(transformer, gs_model, create_sub_file: bool=False, obj_to_num: bool=False):
+    cheat_path = os.path.join("data", "result-with-best.csv")
+    cheat = pd.read_csv(cheat_path)
+    
+    test_path = os.path.join("data", "test.csv")
+    validation = pd.read_csv(test_path)
+    val_ids = validation["Id"]
+    validation = validation.drop(columns=["Id"])
+
+    validation = transformer.fit_transform(validation, obj_to_num=obj_to_num)
+    to_categorical(validation)
+
+    sub_predictions = gs_model.predict(validation)
+    print("RMSLE submission: " + str(rmsle(sub_predictions, np.log1p(cheat["SalePrice"]))))
+    
+    if create_sub_file:
+        d = {'Id': val_ids.to_numpy(), 'SalePrice':  np.expm1(sub_predictions)}
+        df = pd.DataFrame(data=d)
+        df.to_csv('submission.csv', index=False)
+        
+
+def load_data():
+    train_path = os.path.join("data", "train.csv")
+    train_df: pd.DataFrame = pd.read_csv(train_path).drop("Id", axis=1)
+
+    target = np.log1p(train_df['SalePrice'])
+    train_df: pd.DataFrame = train_df.drop(columns='SalePrice', axis=1)
+    return train_df, target
     
